f7252b06aff436cb40e2b2af7fc085eddcd3e598
chenchengshuai <2451260711@qq.com>
2020-10-22 15:35:41 +0800

    Add files via upload

---
+++ MultidimensionalArrayCrossGroup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Time    : 2020/10/21 09:07
+# Author  : None
+# Site    : 
+# File    : MultidimensionalArrayCrossGroup.py
+
+
+class MultidimensionalArrayCrossGroup():
+
+    def calculation(self, arrayStr):
+        columns = self._calculation(arrayStr)
+        column = columns[0]
+        groups = []
+
+        for i in range(len(column)):
+            group = [column[i]]
+            group.extend([item[i % len(item)] for item in columns[1:]])
+            groups.append(group)
+        return groups
+
+    def _calculation(self, arrayStr):
+        l = 1
+        tmp = []
+        for item in reversed(arrayStr):
+            tmp.append([i for i in item for _ in range(l)])
+            l *= len(item)
+        return list(reversed(tmp))
+
+
+
+
+multi_dimensional_array_cross_group = MultidimensionalArrayCrossGroup()
+
+
+
+if __name__ == '__main__':
+
+    test = [['a', 'b'], [1, 2, 3], ['hello', 'world']]
+    for i in multi_dimensional_array_cross_group.calculation(test):
+        print(i)
+
+
+
